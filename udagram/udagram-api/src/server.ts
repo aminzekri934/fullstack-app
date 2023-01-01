7443800291ad73d6d0c071d2b25647822d1ac1fc
aminzekri934 <105649554+aminzekri934@users.noreply.github.com>
2023-01-02 00:01:49 +0100

    main

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -29,7 +29,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
- // app.use(cors());
+  app.use(cors());
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS. 
   app.use(cors({
@@ -48,8 +48,8 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   app.use("/api/v0/", IndexRouter);
 
   // Root URI call
-  app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+  app.get("/", async ( req, res ) => {
+    res.send ("/api/v0/");
   });
 
   // Start the Server
