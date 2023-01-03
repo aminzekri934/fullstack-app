4439530b5c0325ca42ec22479186ebba3f26e34e
aminzekri934 <aminzekri934@gmail.com>
2023-01-03 21:44:08 +0100

    main

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -17,8 +17,7 @@ export const config = {
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
-  dbport:process.env.DB_PORT,
-  port:process.env.PORT,
+  port:process.env.DB_PORT,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
