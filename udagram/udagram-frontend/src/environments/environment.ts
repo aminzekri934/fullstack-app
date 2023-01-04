51b1d486e7186b3b9d23fcd88cbe73493bc3b09f
aminzekri934 <aminzekri934@gmail.com>
2023-01-04 21:07:00 +0100

    main

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-zty5jmbp.us-east-1.elasticbeanstalk.com'
+  apiHost: 'http://udagram-api-dev.eba-zty5jmbp.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
