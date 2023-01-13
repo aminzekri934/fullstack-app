80860e82af4b54bf9bcacbe7eac8e1d5ba6e8b95
aminzekri934 <aminzekri934@gmail.com>
2023-01-13 10:55:54 +0100

    main

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,7 +1,1 @@
-eb init 
-git init
-eb use udagram-api-dev 
-eb setenv PORT=$PORT USERNAME=$USERNAME
-git add . 
-git commit -m 'main' 
-eb deploy
+eb init && git init && eb use udagram-api-dev && git add . && git commit -m 'main' && eb deploy
