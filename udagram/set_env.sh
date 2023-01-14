63da49d8e15d6412d518e6361a8f68a98bd5310d
aminzekri934 <aminzekri934@gmail.com>
2023-01-14 06:56:52 +0100

    main

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,16 +1,16 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=password123
-export POSTGRES_HOST=localhost
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::aminezekri
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=mysecretstring
-export URL=http://localhost:8080
-export PORT=8080
-export USERNAME=aminzekri934
-export DB_PORT=5432
+#export POSTGRES_USERNAME=postgres
+#export POSTGRES_PASSWORD=password123
+#export POSTGRES_HOST=localhost
+#export POSTGRES_DB=postgres
+#export AWS_BUCKET=arn:aws:s3:::aminezekri
+#export AWS_REGION=us-east-1
+#export AWS_PROFILE=default
+#export JWT_SECRET=mysecretstring
+#export URL=http://localhost:8080
+#export PORT=8080
+#export USERNAME=aminzekri934
+#export DB_PORT=5432
 
 /*git config --global user.email 'aminzekri934@gmail.com' && git config --global user.name 'aminzekri934'*/
